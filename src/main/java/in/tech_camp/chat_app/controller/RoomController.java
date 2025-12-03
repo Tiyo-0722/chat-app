4bdb064606769ae388ac6b6fb3b585c1ee783361
Tiyo-0722 <t.yamamoto@infomart.co.jp>
2025-12-03 17:01:45 +0900

    チャットルームの削除機能の実装

--- src/main/java/in/tech_camp/chat_app/controller/RoomController.java
+++ src/main/java/in/tech_camp/chat_app/controller/RoomController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import in.tech_camp.chat_app.custom_user.CustomUserDetail;
@@ -96,6 +97,11 @@ public class RoomController {
         return "rooms/new";
       }
     }
+    return "redirect:/";
+  }
+  @PostMapping("/rooms/{roomId}/delete")
+  public String deleteRoom(@PathVariable("roomId") Integer roomId) {
+    roomRepository.deleteById(roomId);
     return "redirect:/";
   }
 }
