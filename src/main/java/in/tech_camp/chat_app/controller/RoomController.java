855c7318665868a3db2df2b2346ecd3b3206f4d8
Tiyo-0722 <t.yamamoto@infomart.co.jp>
2025-12-03 11:46:46 +0900

    画面遷移の実装

--- src/main/java/in/tech_camp/chat_app/controller/RoomController.java
+++ src/main/java/in/tech_camp/chat_app/controller/RoomController.java
@@ -24,6 +24,7 @@ import in.tech_camp.chat_app.repository.UserRepository;
 import in.tech_camp.chat_app.validation.ValidationOrder;
 import lombok.AllArgsConstructor;
 
+
 @Controller
 @AllArgsConstructor
 public class RoomController {
@@ -32,6 +33,19 @@ public class RoomController {
   private final RoomRepository roomRepository;
 
   private final RoomUserRepository roomUserRepository;
+
+  @GetMapping("/")
+  public String index(@AuthenticationPrincipal CustomUserDetail currentUser, Model model) {
+    UserEntity user = userRepository.findById(currentUser.getId());
+    model.addAttribute("user", user);
+    List<RoomUserEntity> roomUserEntities = roomUserRepository.findByUserId(currentUser.getId());
+    List<RoomEntity> roomList = roomUserEntities.stream()
+        .map(RoomUserEntity::getRoom)
+        .collect(Collectors.toList());
+    model.addAttribute("rooms", roomList);
+    return "rooms/index";
+  }
+  
 
   @GetMapping("/rooms/new")
   public String showRoomNew(@AuthenticationPrincipal CustomUserDetail currentUser, Model model){
